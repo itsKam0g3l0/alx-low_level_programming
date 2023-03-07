fc3ed363251f66337b590b17c6f0067398758cef
kamogelo <kamogeloksa@gmail.com>
2023-03-07 06:37:07 -0800

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 8; i++)
+	for (a = 0; a <= 8; a++)
 	{
-		for (b = a + 1; b <= 9; j++)
+		for (b = a + 1; b <= 9; b++)
 		{
 			putchar(a + '0');
 			putchar(b + '0');
