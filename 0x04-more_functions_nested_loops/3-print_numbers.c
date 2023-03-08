5afd52536199606cb87e9771ac9713eceed1ed63
kamogelo <kamogeloksa@gmail.com>
2023-03-07 16:39:53 -0800

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: numbers
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	_putchar(a + '0');
+	}
+		_putchar('\n');
+}
+
